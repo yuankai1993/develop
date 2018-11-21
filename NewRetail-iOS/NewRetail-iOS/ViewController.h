0edccea9b3707183ca0ee22f6a1c9223026e7482
HJL <hjl@hjldeimac.local>
2018-11-21 16:44:00 +0800

    初始化项目

---
+++ NewRetail-iOS/NewRetail-iOS/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NewRetail-iOS
+//
+//  Created by HJL on 2018/11/21.
+//  Copyright © 2018年 HJL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ NewRetail-iOS/NewRetail-iOS/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  NewRetail-iOS
+//
+//  Created by HJL on 2018/11/21.
+//  Copyright © 2018年 HJL. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
